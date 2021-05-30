e801577ec5e69d8fb4532893755d2b0861184f98
Keisuke Nishimura <mumumu0722@gmail.com>
2021-05-30 20:30:51 +0900

    Fix

--- apps/app1.c
+++ apps/app1.c
@@ -1,13 +1,15 @@
 unsigned long long text_section_top = 0x104000000;
 
 void app1() {
-  // char *str = "Hello from app1\r\n";
+  while (1) {
+    // char *str = "Hello from app1\r\n";
 
-  // TODO: Say hello from here by issuing `puts` syscall
-  // syscall_puts(str);
+    // TODO: Say hello from here by issuing `puts` syscall
+    // syscall_puts(str);
 
-  volatile int i = 100000000;
-  while (i--);
+    volatile int i = 100000000;
+    while (i--);
+  }
 
   asm volatile ("jmp *%0" :: "m"(text_section_top));
 }
